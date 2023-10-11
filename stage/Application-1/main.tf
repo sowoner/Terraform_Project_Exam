95e25c75c0b7f743b74b8e506c1d26bc53ba5868
sowoner <botflow@naver.com>
2023-10-11 10:23:37 +0900

    Remote Backend

---
+++ stage/Application-1/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket  = "myterraform-bucket-state-park"
+    key     = "stage/app1/terraform.tfstate"
+    region  = "ap-northeast-2"
+    profile = "terraform_user"
+    dynamodb_table = "myTerraform-bucket-lock-park"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+  profile = "terraform_user"
+}
